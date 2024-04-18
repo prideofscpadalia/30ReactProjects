ca937591fd9267657cbe8ec655bfd1d5228a7fa7
prideofscpadalia <dippypadalia@gmail.com>
2024-04-18 15:12:51 +0530

    modified

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Button({text,btnClass,onClick,icon,onChange}) {
   return (
-    <button className={`btn ${btnClass} text-center m-1 font-bold `} onClick={onClick}
+    <button className={`btn ${btnClass} text-center m-1 font-bold  bg-black text-white`} onClick={onClick}
     onChange={onChange} >
         {icon} 
         {!text ? "Click Me" : text}</button>
--- src/Project/06-Temperature/Temperature.jsx
+++ src/Project/06-Temperature/Temperature.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import Button from '../../Components/Button'
 export default function Temperature() {
+
+  const [temperature,setTemperature] = useState(0);
+
+  const increaseTemperatue=()=>{
+    setTemperature(temperature +1)
+  }
+  const decreaseTemperatue=()=>{
+    setTemperature(temperature -1)
+  }
   return (
-    <div className='container mt-3 text-center'>
-        <div className="bg-light m-auto " style={{width:250}}>
-            <h1 className='text-dark card'>Temperature</h1>
+    <div className='container mt-3  text-center'>
+        <div className=" card bg-light m-auto shadow-lg" style={{width:200}}>
+            <h1 className= {`font-bold text-light card rounded-circle pt-5 ${temperature > 0 ? "bg-danger font-black text-blue-700" : "bg-info"}`}
+            style={{height:200, width:200, border:"2px solid #666"}}
+            >{temperature}° C</h1>
+            <div className="flex-my-2">
+              <Button text="-" onClick={decreaseTemperatue}/>
+              <Button text="+" onClick={increaseTemperatue}/>
+            </div>
         </div>
     </div>
   )
